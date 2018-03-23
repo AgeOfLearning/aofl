3dce423164db07c95c0cb11025afe728f4362b5d
Arian Khosravi <arian.khosravi@aofl.com>
2018-03-23 14:58:46 -0700

    moved aofl-validate to it's own module

---
+++ aofl-validate/index.js
@@ -0,0 +1,11 @@
+import {isValidEmail} from './src/is-valid-email';
+import {isValidFormat} from './src/is-valid-format';
+import {isRequired} from './src/is-required';
+import AoflValidate from './src/AoflValidate';
+
+export {
+  AoflValidate,
+  isValidEmail,
+  isValidFormat,
+  isRequired
+};
--- polymer-components/aofl-validate/index.js
+++
@@ -1,9 +0,0 @@
-import {isValidEmail} from './validators/isValidEmail';
-import {isValidFormat} from './validators/isValidFormat';
-import {isRequired} from './validators/isRequired';
-
-export {
-  isValidEmail,
-  isValidFormat,
-  isRequired
-};
