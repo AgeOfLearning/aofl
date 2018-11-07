e4c0f6b51ea3db3009d7fa93029966495a8defa4
Arian Khosravi <arian.khosravi@aofl.com>
2018-11-07 10:27:45 -0800

    v1.2.1 (11-07-2018) Bugfixes:   - init     - Switched default repo from ssh to https

--- cli-tools/cli/commands/init-project/js/repo-enumerate.js
+++ cli-tools/cli/commands/init-project/js/repo-enumerate.js
@@ -1,6 +1,5 @@
 const repos = {
-  doc: 'git@gitlab.aofl.com:CoreJS/aofl-example-starter.git',
-  default: 'git@github.com:AgeOfLearning/aofl-starter.git'
+  default: 'https://github.com/AgeOfLearning/aofl-starter.git'
 };
 
 module.exports = {
