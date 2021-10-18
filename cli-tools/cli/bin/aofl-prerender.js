f64d10ec558694ce4c2d4e8b0562d4167f8e28d5
Arian Khosravi <arian.khosravi@aofl.com>
2021-10-18 12:39:53 -0700

    Added additional options

--- cli-tools/cli/bin/aofl-prerender.js
+++ cli-tools/cli/bin/aofl-prerender.js
@@ -4,6 +4,11 @@ const program = require('commander');
 
 program
   .option('--config [path]', 'Specify the path to the config file.')
+  .option('-p, --port [port]', 'Specify port number. Defaults to 3000.')
+  .option('-s, --schema [schema]', 'Specify url schema. Defaults to http.')
+  .option('-h, --host [host]', 'Specify host. Defaults to localhost.')
+  .option('--external', 'Enable this if you want to use your own server.')
+  .option('-t, --timeout', 'How long to wait before processing the page. Defaults to zero')
   .on('--help', () => {
     process.stdout.write(`
   Examples:
@@ -14,7 +19,14 @@ program
   })
   .action(() => {
     const options = program.opts();
-    const prerender = new Prerender(options.config);
+    const settings = {
+      port: options.port || 3000,
+      schema: options.schema || 'http',
+      host: options.host || 'localhost',
+      external: options.external,
+      timeout: options.timeout || 0,
+    };
+    const prerender = new Prerender(options.config, settings);
     prerender.init();
   })
   .parse(process.argv);
--- cli-tools/cli/commands/prerender/index.js
+++ cli-tools/cli/commands/prerender/index.js
@@ -6,11 +6,15 @@ const {Routes} = require('../routes');
 const server = require('./server');
 const prerender = require('./prerender');
 const replaceString = require('../../lib/replace-string');
-/**
- *
- *
- * @author Arian Khosravi <arian.khosravi@aofl.com>
- */
+
+const DEFAULT_SETTINGS = {
+  port: 3000,
+  schema: 'http',
+  host: 'localhost',
+  external: false,
+  timeout: 0
+};
+
 class Prerender {
   /**
    * Creates an instance of Prerender.
@@ -21,7 +25,11 @@ class Prerender {
    * @param {Boolean} profile
    * @param {Boolean} debug
    */
-  constructor(config = '.aofl.js') {
+  constructor(config = '.aofl.js', settings = {}) {
+    this.settings = {
+      ...DEFAULT_SETTINGS,
+      ...settings
+    };
     this.configPath = path.resolve(config);
     this.config = loadConfig(this.configPath);
   }
@@ -41,7 +49,14 @@ class Prerender {
     const promises = [];
     let errors = false;
 
-    const s = await server(this.config);
+    const publicPath = this.config.build.publicPath.replace(/\/$/, '');
+    const rootPath = this.config.build.path.replace(new RegExp(`${publicPath}$`), '');
+    const s = await server(rootPath, {
+      port: this.settings.port,
+      schema: this.settings.schema,
+      host: this.settings.host,
+      external: this.settings.external,
+    });
 
     try {
       for (const routeFile of routeFiles) {
@@ -53,7 +68,7 @@ class Prerender {
           const relRoutePath = route.path.replace(/^\//, '');
           const url = s.url + this.config.build.publicPath + relRoutePath;
           const outputPath = path.join(this.config.build.path, relRoutePath, 'index.html');
-          promises.push(prerender(url, {timeout: 0}).then((html) => {
+          promises.push(prerender(url, {timeout: this.settings.timeout}).then((html) => {
             process.stdout.write(chalk.cyan('Processing... ' + url + '\n'));
             const file = fs.readFileSync(outputPath, 'utf8');
             let source = file;
